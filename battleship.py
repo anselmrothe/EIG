61fb3ff4faaedc225f2c7bf2f2419369d1f68033
anselmrothe <anselmrothe@users.noreply.github.com>
2018-10-11 15:09:05 -0400

    battleship separate from hypothesis

--- battleship.py
+++ battleship.py
@@ -1,8 +1,9 @@
 import numpy
 import unittest
+from hypothesis import Hypothesis
 
 
-class Hypothesis():
+class BattleshipHypothesis(Hypothesis):
 
     def __init__(self, grid_size, ship_id, topleft, size, orientation):
         self.topleft = topleft
@@ -17,8 +18,9 @@ class Hypothesis():
 class Test(unittest.TestCase):
 
     def test_hypothesis(self):
-        hypothesis = Hypothesis(grid_size=3,
-                                ship_id=1, topleft=(1, 1), size=2, orientation='vertical')
+        hypothesis = BattleshipHypothesis(
+            grid_size=3,
+            ship_id=1, topleft=(1, 1), size=2, orientation='vertical')
         board = numpy.array([1, 0, 0, 1, 0, 0, 0, 0, 0]).reshape(3, 3)
         self.assertTrue(numpy.array_equal(hypothesis.board, board))
 
