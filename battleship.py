d9e054fc57d86c7404d80c3f4e525ce43f227622
Ziyun Wang <wang-zy14@mails.tsinghua.edu.cn>
2018-10-11 15:32:08 -0400

    fix typo

--- battleship.py
+++ battleship.py
@@ -32,8 +32,8 @@ class BattleshipHypothesis(Hypothesis):
 class Test(unittest.TestCase):
 
     def test_hypothesis(self):
-        ships = [ship(ship_label=1, topleft=(0, 0), size=3, orientation='vertical'),
-                       ship(ship_label=2, topleft=(0, 1), size=2, orientation='horizontal')]
+        ships = [Ship(ship_label=1, topleft=(0, 0), size=3, orientation='vertical'),
+                       Ship(ship_label=2, topleft=(0, 1), size=2, orientation='horizontal')]
         hypothesis = BattleshipHypothesis(grid_size=3, ships=ships)
         
         board = numpy.array([1, 2, 2, 1, 0, 0, 1, 0, 0]).reshape(3, 3)
