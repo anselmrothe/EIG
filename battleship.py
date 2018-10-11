04d3b92ad768bdc3a2c369315989c5870e54a855
anselmrothe <anselmrothe@users.noreply.github.com>
2018-10-11 15:30:30 -0400

    create_hypothesis_space

--- battleship.py
+++ battleship.py
@@ -3,6 +3,22 @@ import unittest
 from hypothesis import Hypothesis
 
 
+def create_hypothesis_space(grid_size, ships, shipsizes, orientations):
+    topleft_positions = [(row, column) for row in range(grid_size)
+                         for column in range(grid_size)]
+    hypotheses = []
+    for ship in ships:
+        for shipsize in shipsizes:
+            for topleft in topleft_positions:
+                for orientation in orientations:
+                    hypothesis = BattleshipHypothesis(grid_size=grid_size,
+                                                      ship_label=ship,
+                                                      topleft=(0, 0), size=2,
+                                                      orientation=orientation)
+                    hypotheses.append(hypothesis)
+    return(hypotheses)
+
+
 class BattleshipHypothesis(Hypothesis):
 
     def __init__(self, grid_size, ship_id, topleft, size, orientation):
@@ -23,6 +39,9 @@ class Test(unittest.TestCase):
             ship_id=1, topleft=(1, 1), size=2, orientation='vertical')
         board = numpy.array([1, 0, 0, 1, 0, 0, 0, 0, 0]).reshape(3, 3)
         self.assertTrue(numpy.array_equal(hypothesis.board, board))
+
+    def test_create_hypothesis_space(self):
+        create_hypothesis_space(grid_size=3, ships=[1, 2], shipsizes=[3])
 
 if __name__ == '__main__':
     unittest.main()
