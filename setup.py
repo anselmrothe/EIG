3c383a62f5de417854563f33e93aa01faa14386b
Ziyun Wang <wang-zy14@mails.tsinghua.edu.cn>
2018-10-28 01:05:20 -0400

    different compile args using clang/g++

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ from Cython.Build import cythonize
 from setuptools import Extension
 import numpy
 import os
+from distutils.spawn import find_executable
 
 cwd = os.getcwd()
 compile_args = {
     "include_dirs": [numpy.get_include(), cwd + "/eig/battleship/cpp"],
-    "extra_compile_args": ["-std=c++11", "-stdlib=libc++"],
-    "extra_link_args": ["-std=c++11", "-stdlib=libc++"]
+    "extra_compile_args": ["-std=c++11"],
+    "extra_link_args": ["-std=c++11"]
 }
+
+clang_executable = find_executable('clang++')
+if clang_executable is None:
+    os.environ["CC"] = "g++"
+    os.environ["CXX"] = "g++"
+else:
+    os.environ["CC"] = "clang++"
+    os.environ["CXX"] = "clang++"
+    compile_args["extra_compile_args"].append("-stdlib=libc++")
+    compile_args["extra_link_args"].append("-stdlib=libc++")
 
 extensions = [
     Extension("eig.battleship.question.executor",
