3ddd24088402014c7c686c4f964a3a85952e55e5
Ziyun Wang <wang-zy14@mails.tsinghua.edu.cn>
2018-11-09 17:41:59 -0500

    add some comments

--- eig/battleship/python/executor.py
+++ eig/battleship/python/executor.py
@@ -6,22 +6,26 @@ class Executor:
         self._executor = self._build_executor(question)
 
     def _traverse_ast(self, hypothesis, node, lambda_v=None):
-        # TODO: some comments needed
-
+        # for leaf nodes, return its value 
         if node.children is None:
             if node.ntype.startswith("lambda_"):
                 return lambda_v
             else: return node.value
+        
+        # for lambdas, return a wrapper function 
         if node.ntype == "lambda_op":
             def lambda_f(x):
                 return self._traverse_ast(hypothesis, node.children[1], x)
             return lambda_f 
 
+        # for other functions, evaluate its arguments first, and execute the function
         arguments = []
         if node.ntype.endswith("_fn"):
+            # first argument of board functions is the hypothesis
             arguments.append(hypothesis)
         for c in node.children:
             arguments.append(self._traverse_ast(hypothesis, c, lambda_v))
+        # call the function and return its result
         return getattr(F, node.ntype)(node, *arguments)
 
     def _build_executor(self, question):
